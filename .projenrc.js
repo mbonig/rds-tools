b33114548b2efdc1bf63ff380545a91d346e3aae
Matthew Bonig <matthew.bonig@gmail.com>
2021-02-13 10:54:21 -0700

    fixing deps issue

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,17 @@
 const { AwsCdkConstructLibrary } = require('projen');
 
+let cdkDeps = [
+  '@aws-cdk/core',
+  '@aws-cdk/aws-ec2',
+  '@aws-cdk/aws-rds',
+  '@aws-cdk/aws-events',
+  '@aws-cdk/aws-events-targets',
+  '@aws-cdk/aws-lambda',
+  '@aws-cdk/aws-lambda-nodejs',
+  '@aws-cdk/aws-logs',
+  '@aws-cdk/aws-rds',
+  '@aws-cdk/aws-secretsmanager',
+];
 const project = new AwsCdkConstructLibrary({
   author: 'Matthew Bonig',
   authorAddress: 'matthew.bonig@gmail.com',
@@ -9,30 +21,11 @@ const project = new AwsCdkConstructLibrary({
   jsiiFqn: 'matthewbonig.rdsTools',
   name: '@matthewbonig/rds-tools',
   repositoryUrl: 'https://github.com/mbonig/rds-tools.git',
-  cdkDependencies: [
-    '@aws-cdk/aws-events',
-    '@aws-cdk/aws-events-targets',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-lambda-nodejs',
-    '@aws-cdk/aws-logs',
-    '@aws-cdk/aws-rds',
-    '@aws-cdk/aws-secretsmanager',
-  ],
+  cdkDependencies: cdkDeps,
   bundledDeps: ['mssql'],
   deps: [],
   devDeps: ['eslint', 'esbuild'],
-  peerDeps: [
-    '@aws-cdk/core',
-    '@aws-cdk/aws-ec2',
-    '@aws-cdk/aws-rds',
-    '@aws-cdk/aws-events',
-    '@aws-cdk/aws-events-targets',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-lambda-nodejs',
-    '@aws-cdk/aws-logs',
-    '@aws-cdk/aws-rds',
-    '@aws-cdk/aws-secretsmanager',
-  ],
+  peerDeps: cdkDeps,
 });
 
 project.synth();
